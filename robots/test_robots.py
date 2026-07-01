c8c482ee0a0f16e72834ec4db71ed98cf06237ab
Energy Robots <planetaryinhabitant@yandex.ru>
2026-07-01 18:34:42 +0300

    Update test_robots.py
    Add corrections in /robots/test_robots.py

--- robots/test_robots.py
+++ robots/test_robots.py
@@ -9,6 +9,29 @@ class TestChemicalProperties(unittest.TestCase):
   # Reactions between the reagents
   # TODO: check test before first start, and do in after creating source of classes Creature: and Spider:
   def test_add_connection(self):
+    spider = Spider()
+    # Programm to creating molecules 
+    creating_molecular_spider = spider.init(chemical=[
+      (
+        [(13, 13), (13, 13)], 
+       [(17, 17), (17, 17), (17, 17)]
+      ), 
+    ),
+    # Program to creating microuniverse-space to molecular reactions 
+    creating_micro_universe_space_reactions = spider.init(perimeter=(100,100,100), chemical =
+                    [
+                      (
+                       (13, 17), (13, 17), (13, 17)
+                      ), 
+                      (
+                       (13, 17), (13, 17), (13, 17)
+                      )
+                    ]
+                   )
+    
+    creature = Creature(spider)
+    connections = creature.get_connections() 
+    ```
     self.assertEqual(Creature(Spider(chemical=[
       (
         [(13, 13), (13, 13)], 
@@ -23,5 +46,8 @@ class TestChemicalProperties(unittest.TestCase):
                        (13, 17), (13, 17), (13, 17)
                       )
                     ]
-      )
+                    )
+    ```
+
+
 
